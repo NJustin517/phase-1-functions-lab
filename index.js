198badd8731f5d0df44755a73db37bf16a6ffdb8
Justin Luciano <justinlluciano@gmail.com>
2021-12-13 17:53:19 -0800

    commit tree 0a9cea353904427f2f754f1b2560821bd79bd95f

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const hq = 42;
+
+function distanceFromHqInBlocks(destination) {
+  if ((destination - hq) >= 0) {
+    return destination - hq;
+  } else {
+    return hq - destination;
+  }
+}
+
+function distanceFromHqInFeet(destination) {
+  return distanceFromHqInBlocks(destination) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if ((destination - start) >= 0) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  if (distance < 400) {
+    return 0;
+  } else if (distance <= 2000) {
+    return (distance - 400) *.02;
+  } else if (distance < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far"
+  }
+}
